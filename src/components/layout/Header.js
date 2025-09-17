5bffdcb336e49e901ce20fab7d955ffda434dbe0
Sebastian Perez <sebastianperezescobedo@gmail.com>
2025-09-16 22:30:33 -0500

    modified la funcionalidad del Header.js y Header.css

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,17 +2,19 @@ import React from 'react';
 import './css/Header.css';
 import SearchBar from '../common/SearchBar';
 import Avatar from '../common/Avatar';
+import { FaBars, FaTimes, FaUpload, FaBell } from 'react-icons/fa';
 
-const Header = ({ onMenuToggle }) => {
+
+const Header = ({ onMenuToggle, isOpen }) => {
   return (
     <header className="header">
-      <button className="menu-toggle" onClick={onMenuToggle}>
-        <i className="fas fa-bars"></i>
+      <button className="menu-toggle" onClick={onMenuToggle} aria-label="Toggle menu">
+        {isOpen ? <FaTimes /> : <FaBars />}
       </button>
       <SearchBar />
       <div className="user-actions">
-        <button><i className="fas fa-upload"></i></button>
-        <button><i className="far fa-bell"></i></button>
+        <button><FaUpload /></button>
+        <button><FaBell /></button>
         <div className="user-profile">
           <Avatar size="small" />
           <span className="user-name">UsuarioEjemplo</span>
